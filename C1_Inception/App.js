ae2ca323b339e809c81c0e29d7b1e95a22cf2acb
khemchand7 <khemchandk.@gmail.com>
2024-03-14 10:46:41 +0530

    class 1 Inception

---
+++ C1_Inception/App.js
@@ -0,0 +1,12 @@
+/* 
+
+
+*/
+
+
+const parent=React.createElement("h1",{id:"parent"},
+   "i am a h1 tag");
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);//we are adding h1 tag present in div having id root in html
